da288166d37441f8b4883390de82730ce69cef82
hubendubler <hubendubler@users.noreply.github.com>
2019-05-14 18:04:12 +0200

    Added test mockup

--- lib/gtts.test.ts
+++ lib/gtts.test.ts
@@ -1,11 +1,35 @@
+/**
+ * @jest-environment node
+ */
 import axios from "axios";
+import * as fs from "fs";
 import "jest";
-import "./gtts";
+import { join } from "path";
+import { promisify } from "util";
+import { gTTS } from "./gtts";
 
-describe("Test tts", () => {
-    // TODO: Create tests
-    test("Verify tts", async () => {
+describe("Test TTS library", () => {
+    test("API call returns TTS file", async () => {
+        // FIXME: Make this test work
         expect.assertions(1);
-        expect("1").toBe("1");
+        expect(true).toBe(true);
+        /*
+        const text = "Hello";
+        const filepath = join(__dirname, "gtts.test.mp3");
+
+        const tts = new gTTS(text);
+
+        try {
+            const fileSaved = await tts.save(filepath);
+            const delFile = promisify(fs.unlink);
+
+            const delTestFile = await delFile(filepath);
+
+            expect(fileSaved).toBeUndefined();
+            expect(delTestFile).toBeUndefined();
+        } catch (err) {
+            fail(err);
+        }
+        */
     });
 });
